73c3ac62efa83274d3ea909624bf63bbb3599cb8
sebasarias17 <ariasusmasebastian@gmail.com>
2024-08-16 08:27:07 -0500

    valid parenthesis finished

--- valid_parenthesis.py
+++ valid_parenthesis.py
@@ -1,24 +1,19 @@
 class Solution(object):
     def longestValidParentheses(self, s):
-        arr = [*s]
-        counter = 0
-        arr2 = []
+        stack = [-1]
+        max_length = 0
         
-        while counter < len(arr):
-            if arr[counter] == '(':
-                if counter + 1 < len(arr) and arr[counter + 1] == ')':
-                    arr2.append(arr[counter])  # Añade '('
-                    arr2.append(arr[counter + 1])  # Añade ')'
-                    counter += 2  # Salta la pareja de paréntesis
-                else:
-                    counter += 1  # Si no hay un ')' siguiente, solo avanza
+        
+        for i,char in enumerate(s):
+            if char == '(':
+                stack.append(i)
             else:
-                counter += 1  # Si no es '(', solo avanza
-        
-        max_length = len(arr2)  
-        print("arreglo original:", arr)
-        print("arreglo nuevo:", arr2)
-        return max_length
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    max_length = max(max_length, i - stack[-1])
+        return (max_length)
     
 def main():
     s = "(()())" 
