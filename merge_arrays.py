1fedf2b1db7b554191211d58db7d56dfa1fd4411
sebasarias17 <ariasusmasebastian@gmail.com>
2024-08-18 23:31:24 -0500

    Mejor version del merge arrays

--- merge_arrays.py
+++ merge_arrays.py
@@ -1,22 +1,26 @@
 class Solution(object):
     def mergeArrays(self, nums1, nums2):
-        def merge_recursive(i, j):
-            if i == len(nums1):
-                return nums2[j:]
-            if j == len(nums2):
-                return nums1[i:]
-            
+        i,j = 0,0
+        result = []
+        
+        while i < len(nums1) and j < len(nums2):
             id1, val1 = nums1[i]
             id2, val2 = nums2[j]
-            
             if id1 == id2:
-                return [[id1, val1 + val2]] + merge_recursive(i + 1, j + 1)
+                result.append([id1, val1 + val2])
+                i +=1
+                j +=1
             elif id1 < id2:
-                return [[id1, val1]] + merge_recursive(i + 1, j)
+                result.append([id1, val1])
+                i +=1
             else:
-                return [[id2, val2]] + merge_recursive(i, j + 1)
+                result.append([id2, val2])
+                j +=1
 
-        return merge_recursive(0, 0)
+        result.extend(nums1[i:])
+        result.extend(nums2[j:])
+
+        return result
 
 def main():
     nums1 = [[1, 2], [2, 3], [4, 5]]
@@ -24,10 +28,7 @@ def main():
 
     solution = Solution()
     result = solution.mergeArrays(nums1, nums2)
-
     print("INPUTS: ", "nums1: ", nums1, "," , "nums2: ", nums2 )
     print("Resultado:", result)
-
-
 if __name__ == "__main__":
     main()
