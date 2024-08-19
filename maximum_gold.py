3a3cdbdce38b51198d5075e9f841ff4d90c582a1
sebasarias17 <ariasusmasebastian@gmail.com>
2024-08-18 23:46:02 -0500

    maximum gold terminado y optimizado

--- maximum_gold.py
+++ maximum_gold.py
@@ -2,30 +2,25 @@ class Solution:
     def getMaximumGold(self, grid):
         numRows, numCols = len(grid), len(grid[0])
 
-        def dfs(row, col, visited):
-
-            if row < 0 or col < 0:
-                return 0
-            if row >= numRows or col >= numCols:
+        def dfs(row, col):
+            if row < 0 or col < 0 or row >= numRows or col >= numCols or grid[row][col] == 0:
                 return 0
 
-            if grid[row][col] == 0:
-                return 0
+            original_value = grid[row][col]
+            grid[row][col] = 0
 
-            if (row, col) in visited:
-                return 0
+            maxGold = 0
+            for r, c in [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]:
+                maxGold = max(maxGold, dfs(r, c))
 
-            visited.add((row, col))
-            gold = grid[row][col]
-            direction_moves = [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]
-            for nextRow, nextCol in direction_moves:
-                gold = max(gold, grid[row][col] + dfs(nextRow, nextCol, visited))
-            visited.remove((row, col))
-            return gold
+            grid[row][col] = original_value
+
+            return maxGold + original_value
 
         maxGold = 0
         for row in range(numRows):
             for col in range(numCols):
                 if grid[row][col] != 0:
-                    maxGold = max(maxGold, dfs(row, col, set()))
+                    maxGold = max(maxGold, dfs(row, col))
+
         return maxGold
